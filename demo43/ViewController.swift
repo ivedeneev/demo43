332cf01d873a698e96980abc10844337413bdd33
Igor Vedeneev <i.vedeneev@agima.ru>
2018-07-26 10:36:08 +0300

    add table view

--- demo43/ViewController.swift
+++ demo43/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupTableView() {
+        tableView.frame = view.bounds
+        view.addSubview(tableView)
+    }
 }
 
